72a54615a7dc11840ab9719c243b9596357c48e9
Yvonne-Rochester <yrochester@gmail.com>
2025-01-05 03:40:12 -0500

    Update website displays.js
    Added coding for new products

--- website displays.js
+++ website displays.js
@@ -6,6 +6,10 @@ function redirectToProduct(productId) {
         'product2': 'https://amzn.to/41sqL6J', // Ring Video Doorbell Pro
         'product3': 'https://amzn.to/4gu5XQs', // Apple AirPods Pro 2
         'product4': 'https://amzn.to/4gcBbvy', // Echo Dot (5th Gen) with Alexa
+        'product5': 'https://amzn.to/406PcV3', // Amazon Beats Studio Pro
+        'product6': 'https://amzn.to/3DIZMtG', // Bose QuietComfort Bluetooth Headphones
+        'product7': 'https://amzn.to/3W5xtMv', // USB Wall Charger, Surge Protector
+        'product8': 'https://amzn.to/4001fmZ', // JBL Go 3 Portable Mini Bluetooth Speaker
     };
 
     if (productLinks[productId]) {
